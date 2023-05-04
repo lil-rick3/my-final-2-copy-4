31c103d5ae51a220bcc4b3c1b4ce90462ad057d3
Berny Peralta <jbm3@arizona.edu>
2023-05-03 22:20:57 -0700

    webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  devServer: {
+    allowedHosts: ["localhost", "159.223.182.236"],
+    // other dev server options
+  },
+  // other webpack config options
+};
