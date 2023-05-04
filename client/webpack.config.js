15a65877b24fdc232835d42edc9ca344c19a1247
Berny Peralta <jbm3@arizona.edu>
2023-05-03 22:28:29 -0700

    remove webpack

--- client/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  devServer: {
-    allowedHosts: ["localhost", "159.223.182.236"],
-    // other dev server options
-  },
-  // other webpack config options
-};
